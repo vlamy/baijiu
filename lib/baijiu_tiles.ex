fe9b3b5a344be7de30fe7b85a675873550cad7f2
Willy Malvault <wmalvault@leadformance.com>
2018-01-17 21:54:21 +0100

    initial commit

---
+++ lib/baijiu_tiles.ex
@@ -0,0 +1,18 @@
+defmodule BaijiuTiles do
+  @moduledoc """
+  Documentation for BaijiuTiles.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BaijiuTiles.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/baijiu_tiles_test.exs
@@ -0,0 +1,8 @@
+defmodule BaijiuTilesTest do
+  use ExUnit.Case
+  doctest BaijiuTiles
+
+  test "greets the world" do
+    assert BaijiuTiles.hello() == :world
+  end
+end
