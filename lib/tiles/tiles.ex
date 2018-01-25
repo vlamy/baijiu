ba2ef0df40304b06e132e14472d34151e158577e
Willy Malvault <wmalvault@leadformance.com>
2018-01-25 22:24:47 +0100

    feat(tiles): full random hand generator

--- lib/tiles/tiles.ex
+++ lib/tiles/tiles.ex
@@ -23,7 +23,7 @@ defmodule Baijiu.Tiles do
     :bamboo_6 => %Tile{name: "Six of Bamboo", family: @families.bamboo, number: 6},
     :bamboo_7 => %Tile{name: "Seven of Bamboo", family: @families.bamboo, number: 7},
     :bamboo_8 => %Tile{name: "Eight of Bamboo", family: @families.bamboo, number: 8},
-    :bamboo_9 => %Tile{name: "Nine of Bamboo", family: @families.bamboo, number: 9}
+    :bamboo_9 => %Tile{name: "Nine of Bamboo", family: @families.bamboo, number: 9},
   }
 
   @character_tiles %{
@@ -35,7 +35,7 @@ defmodule Baijiu.Tiles do
     :character_6 => %Tile{name: "Six of Character", family: @families.character, number: 6},
     :character_7 => %Tile{name: "Seven of Character", family: @families.character, number: 7},
     :character_8 => %Tile{name: "Eight of Character", family: @families.character, number: 8},
-    :character_9 => %Tile{name: "Nine of Character", family: @families.character, number: 9}
+    :character_9 => %Tile{name: "Nine of Character", family: @families.character, number: 9},
   }
 
   @circle_tiles %{
@@ -47,6 +47,46 @@ defmodule Baijiu.Tiles do
     :circle_6 => %Tile{name: "Six of Circle", family: @families.circle, number: 6},
     :circle_7 => %Tile{name: "Seven of Circle", family: @families.circle, number: 7},
     :circle_8 => %Tile{name: "Eight of Circle", family: @families.circle, number: 8},
-    :circle_9 => %Tile{name: "Nine of Circle", family: @families.circle, number: 9}
+    :circle_9 => %Tile{name: "Nine of Circle", family: @families.circle, number: 9},
   }
+
+  @dragon_tiles %{
+    :green_dragon => %Tile{name: "Green dragon", family: @families.dragon, number: 1},
+    :red_dragon => %Tile{name: "Red dragon", family: @families.dragon, number: 2},
+    :white_dragon => %Tile{name: "White dragon", family: @families.dragon, number: 3},
+  }
+
+  @wind_tiles %{
+    :east_wind => %Tile{name: "East wind", family: @families.wind, number: 1},
+    :north_wind => %Tile{name: "North wind", family: @families.wind, number: 4},
+    :south_wind => %Tile{name: "South wind", family: @families.wind, number: 2},
+    :west_wind => %Tile{name: "West wind", family: @families.wind, number: 3},
+  }
+
+
+  @all_tiles_map @bamboo_tiles |> Map.merge(@character_tiles)
+    |> Map.merge(@circle_tiles)
+    |> Map.merge(@dragon_tiles)
+    |> Map.merge(@wind_tiles)
+
+  @all_tiles_tuple @all_tiles_map |> Map.to_list |> List.to_tuple
+
+  @number_of_different_tiles tuple_size(@all_tiles_tuple)
+
+  def all_tiles_map, do: @all_tiles_map
+  def all_tiles_tuple, do: @all_tiles_tuple
+  def number_of_tiles, do: @number_of_different_tiles
+
+  def get_random_tile do
+    # elem(@all_tiles_tuple, Baijiu.Utils.random(@number_of_different_tiles))
+    elem(all_tiles_tuple(), Baijiu.Utils.random(number_of_tiles()))
+  end
+
+  def random_tiles_tuple(list_size) when list_size == 0, do: {}
+
+  def random_tiles_tuple(list_size) when list_size > 0 do
+    Tuple.append(random_tiles_tuple(list_size - 1), get_random_tile())
+  end
+
+  def full_random_hand, do: random_tiles_tuple(14)
 end
---
+++ lib/utils.ex
@@ -0,0 +1,18 @@
+defmodule Baijiu.Utils do
+  @moduledoc """
+  Module hosting utils functions for Baijiu
+  """
+
+  @default_random_size 1024
+
+  @doc """
+  Return a random number between 0 and `size`
+  see https://hashrocket.com/blog/posts/the-adventures-of-generating-random-numbers-in-erlang-and-elixir
+
+  """
+  def random(size \\ @default_random_size) do
+    <<i1 :: unsigned-integer-32, i2 :: unsigned-integer-32, i3 :: unsigned-integer-32>> = :crypto.strong_rand_bytes(12)
+    :rand.seed(:exsplus, {i1, i2, i3})
+    :rand.uniform(size) # generate random number from 0 - size
+  end
+end
